f8358f7f03637f513ba078c95f0474383c359f8c
CoolDark <apnkel@gmail.com>
2014-01-29 00:38:50 -0800

    Fixed command pay

--- command/player_s.lua
+++ command/player_s.lua
@@ -1,3 +1,10 @@
+--
+--The MIT License (MIT)
+--Copyright (c) 2014 CoolDark
+--
+--See LICENSE file
+--
+
 addCommandHandler ( "pm",
 	function ( player, cmd, id, ... )
 		local tableText = { ... }
@@ -18,17 +25,21 @@ addCommandHandler ( "pm",
 addCommandHandler ( "pay",
 	function ( player, cmd, id, amount )
 		if tonumber ( amount ) then
-			if player ~=  g_Players[tonumber (id)] then
-				if g_Players[tonumber (id)] ~= nil then
-					takePlayerMoney ( player, amount )
-					outputChatBox ( "#F2F200You paid #08F200".. getPlayerName (g_Players[tonumber (id)]) .." #F2F200: " .. amount .. "$", player, 255, 255, 255, true )
-					givePlayerMoney ( g_Players[tonumber (id)], amount )
-					outputChatBox ( "#F2F200You got money from #08F200".. getPlayerName ( player ) .." #F2F200: " .. amount .. "$", g_Players[tonumber (id)], 255, 255, 255, true )
+			if (tonumber(amount) > 0 and getPlayerMoney(player) >= tonumber(amount)) then
+				if player ~=  g_Players[tonumber (id)] then
+					if g_Players[tonumber (id)] ~= nil then
+						takePlayerMoney ( player, amount )
+						outputChatBox ( "#F2F200You paid #08F200".. getPlayerName (g_Players[tonumber (id)]) .." #F2F200: " .. amount .. "$", player, 255, 255, 255, true )
+						givePlayerMoney ( g_Players[tonumber (id)], amount )
+						outputChatBox ( "#F2F200You got money from #08F200".. getPlayerName ( player ) .." #F2F200: " .. amount .. "$", g_Players[tonumber (id)], 255, 255, 255, true )
+					else
+						outputChatBox ( "#F20010[Syntax]: /pay id amount", player, 255, 255, 255, true )
+					end
 				else
-					outputChatBox ( "#F20010[Syntax]: /pay id amount", player, 255, 255, 255, true )
+					outputChatBox ( "#F20010You can not pay yourself", player, 255, 255, 255, true  )
 				end
 			else
-				outputChatBox ( "#F20010You can not pay yourself", player, 255, 255, 255, true  )
+				outputChatBox ( "#F20010You do not have enough money", player, 255, 255, 255, true  )
 			end
 		else
 			outputChatBox ( "#F20010[Syntax]: /pay id amount", player, 255, 255, 255, true )
